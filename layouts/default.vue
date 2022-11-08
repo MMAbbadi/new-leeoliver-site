09783331f7df8855d32df0206f02cb7a633a09db
MMAbbadi <mmabbadi99@gmail.com>
2022-11-07 21:43:24 -0300

    [continuacao]

---
+++ layouts/default.vue
@@ -0,0 +1,54 @@
+<template>
+ <div>
+    <header>
+        <div class="cabecalho" id="cabecalho" >
+            <NuxtLink to="Home">Home</NuxtLink>
+            <NuxtLink to="Clipes">Clipes</NuxtLink>
+            <div class="logo"><img src="" alt="">LeeOliver.com</div>
+            <NuxtLink to="Sobre">Sobre</NuxtLink>
+            <NuxtLink to="Contato">Contato e Midias</NuxtLink>
+        </div>
+    </header>
+
+    <Nuxt />
+
+    <footer  id="rodape">
+        <div class="rodape">
+            <p>Me siga nas redes sociais e na TwitchTV para saber quando entrar ao vivo!</p>
+        </div>
+    </footer>
+ </div>
+    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+*{
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+}
+#cabecalho{
+    background-color: green;
+    justify-content: center;
+    align-content: center;
+    display: flex;
+    height: 50px;
+    padding: 11px;
+    gap: 15px;
+}
+
+#rodape{
+    background-color: green;
+    height: 55px;
+    display: flex;
+    align-content: center;
+    justify-content: center;
+    padding: 11px;
+}
+</style>
---
+++ pages/Home.vue
@@ -0,0 +1,70 @@
+<template>
+    <div>
+        <div class="streams">
+
+            <div id="btnvoltar" class="botao">
+                <button class="voltar" @click="">
+                    voltar
+                </button>
+            </div>
+
+            <ul class="lives">
+                <li class="livestream">
+                    <h3 class="titulo-live">V1</h3>
+                    <video controls src=""></video>
+                </li>
+                <li class="livestream">
+                    <h3 class="titulo-live">V2</h3>
+                    <video controls src=""></video>
+                </li>
+                <li class="livestream">
+                    <h3 class="titulo-live">V3</h3>
+                    <video controls src=""></video>
+                </li>
+            </ul>
+
+            <div id="btnavancar" class="botao">
+                <button class="avancar" @click="">
+                    avancar
+                </button>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+ul{
+    list-style: none;
+}
+
+.streams{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 15px;
+}
+
+.lives{
+    height: 864px;
+    width: 960px;
+    position: relative;
+}
+
+.livestream{
+    height: 100%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    position: absolute;
+}
+
+</style>
